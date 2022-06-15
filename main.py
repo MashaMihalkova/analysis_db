eb8b77e675b9b6ddde0f71797896ee7b002bd850
Masha_Mihalkova <marusia_mi@mail.ru>
2022-06-15 17:38:36 +0300

    'fixed writing to file'

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import psycopg2
 from multipledispatch import dispatch
 import json
 import math
+import os
 
 
 class TYPE_Errors(object):
@@ -25,7 +26,8 @@ class TYPE_Errors(object):
         self.dict_json = {}
         # self.data = {}
         self.data = {'Very big value': [], 'Very small value': [], 'Wrong sign': [], 'Small data': [], 'Big data': [],
-                     'Error span year': []}
+                     'Error span year': [], 'Mismatch of values in two tables (new value is lower then old)': [],
+                     'Dublicate rows in resassagment': []}
 
     def find_max_type(self, type_columns_):
         dict_type_column = dict(zip(type_columns_, [type_columns_.count(i) for i in type_columns_]))
@@ -36,23 +38,9 @@ class TYPE_Errors(object):
 
     def write_to_dict(self):
         # self.dict_json = 0
-        with open('log/data_3.txt', 'a+') as outfile:
+        with open('/'.join((PATH_TO_LOG, 'log.txt')), 'a+') as outfile:
             json.dump(self.data, outfile)
             outfile.write(', \n')
-
-    def write_to_dict_begin(self):
-        # self.dict_json = 0
-        with open('log/data_3.txt', 'a+') as outfile:
-            # json.dump(self.data, outfile)
-            outfile.write('[')
-            outfile.write('\n')
-
-    def write_to_dict_end(self):
-        # self.dict_json = 0
-        with open('log/data_3.txt', 'a+') as outfile:
-            # json.dump(self.data, outfile)
-            outfile.write(']')
-            outfile.write('\n')
 
     def func_all_int_float_array(self, column1, type_columns) -> list:
         return [column1[i] for i, value in enumerate(column1) if
@@ -153,113 +141,95 @@ class TYPE_Errors(object):
                 logger.warning(
                     f'warning small year in cell : column = {column_index} row = {i}')
 
-    def print_warning_in_two_table(self, df, i, name_table, column1, j, one_table, PK):
-        print(f'name column = {df.columns[i]}')
-        if not one_table:
-            if type(PK) == str:
-                print(f'PK {PK} = {df[PK].values[j][0]}')
-            else:
-                print(f'PK {PK[0]} = {df[PK[0]].values[j][0]}')
-                print(f'PK2 {PK[1]} = {df[PK[1]].values[j][0]}')
-            print(f'{name_table} value = {column1[j][i]}')
-            print(f'{name_table}_pmc value = {column1[j][i + (df.shape[1]) // 2]} \n')
+    def conv_type_for_json(self, column1, name_table, two_pk):
+        dict_value = {}
+        if two_pk:
+            print(f'value = {column1[0]}')
+            print(f'value_pmc = {column1[1]}')
+            if type(column1[0]) == int:
+                dict_value = {
+                    f'value': int(column1[0]),
+                    f'pmc value': int(column1[1])}
+            elif type(column1[0]) == float or type(column1[0]) == np.float64 or type(column1[0]) == np.float32:
+                dict_value = {
+                    f'value': float(column1[0]),
+                    f'pmc value': float(column1[1])}
         else:
-            if type(PK) == str:
-                print(f'PK {PK} = {df[PK].values[j]}')
-            else:
-                print(f'PK {PK[0]} = {df[PK[0]].values[j]}')
-                print(f'PK2 {PK[1]} = {df[PK[1]].values[j]}')
-            print(f'{name_table} value = {column1}')
+            print(f'value = {column1}')
+            if type(column1) == np.float64 or type(column1) == np.float32 or type(column1) == float:
+                dict_value = {f'value': float(column1)}
+            elif type(column1) == np.int:
+                dict_value = {f'value': int(column1)}
+
+        return dict_value
 
     def print_name_table_row_PK(self, df, i, name_table, column1, j, one_table, PK, type_error):
-        # self.data[f'{type_error}'] = []
-        # self.data[f'{type_error}'].append({
-        #     self.dict_json['name_table']: name_table,
-        #     self.dict_json['name_column']: df.columns[i]})
-        self.data[f'{type_error}'].append({
-            'name_table': name_table,
-            'name_column': df.columns[i]})
+        dict_ = {'name_table': name_table,
+                 'name_column': df.columns[i]}
+        dict_PK = {}
+        dict_value = {}
         print(f'name column = {df.columns[i]}')
         if not one_table:
             if type(PK) == str:
                 print(f'PK {PK} = {df[PK].values[j][0]}')
-                # self.data[f'{type_error}'].append({self.dict_json['PK']: int(df[PK].values[j][0])})
-                self.data[f'{type_error}'].append({'PK': int(df[PK].values[j][0])})
+                dict_PK = {'PK': int(df[PK].values[j][0])}
             else:
                 print(f'PK {PK[0]} = {df[PK[0]].values[j][0]}')
                 print(f'PK2 {PK[1]} = {df[PK[1]].values[j][0]}')
-                # self.data[f'{type_error}'].append({
-                #     self.dict_json[f'PK {PK[0]}']: int(df[PK[0]].values[j][0]),
-                #     self.dict_json[f'PK {PK[1]}']: datetime.date(df[PK[1]].values[j][0])})
-                self.data[f'{type_error}'].append({
+                dict_PK = {
                     PK[0]: int(df[PK[0]].values[j][0]),
-                    PK[1]: str(df[PK[1]].values[j][0])})
+                    PK[1]: str(df[PK[1]].values[j][0])}
             print(f'{name_table} value = {column1[j][i]}')
             print(f'{name_table}_pmc value = {column1[j][i + (df.shape[1]) // 2]} \n')
-            # TODO: не только int но и float
-            # self.data[f'{type_error}'].append({
-            #     self.dict_json[f'{name_table} value']: int(column1[j][i]),
-            if type(column1[j][i]) == int:
-                self.data[f'{type_error}'].append({
-                    f'{name_table} value': int(column1[j][i]),
-                    f'{name_table}_pmc value': int(column1[j][i + (df.shape[1]) // 2])})
-            elif type(column1[j][i]) == float or type(column1[j][i]) == np.float64 or type(column1[j][i]) == np.float32:
-                self.data[f'{type_error}'].append({
-                    f'{name_table} value': float(column1[j][i]),
-                    f'{name_table}_pmc value': float(column1[j][i + (df.shape[1]) // 2])})
-
+            values = [column1[j][i], column1[j][i + (df.shape[1]) // 2]]
+            dict_value = self.conv_type_for_json(values, name_table, True)
         else:
             if type(PK) == str:
                 print(f'PK {PK} = {df[PK].values[j]}')
-                # self.data[f'{type_error}'].append({self.dict_json['PK']: int(df[PK].values[j])})
-                self.data[f'{type_error}'].append({'PK': int(df[PK].values[j])})
+                dict_PK = {'PK': int(df[PK].values[j])}
             else:
                 print(f'PK {PK[0]} = {df[PK[0]].values[j]}')
                 print(f'PK2 {PK[1]} = {df[PK[1]].values[j]}')
-                # self.data[f'{type_error}'].append({
-                #     self.dict_json[f'PK {PK[0]}']: int(df[PK[0]].values[j]),
-                #     self.dict_json[f'PK {PK[1]}']: datetime.date(df[PK[1]].values[j])})
-                self.data[f'{type_error}'].append({
+                dict_PK = {
                     PK[0]: int(df[PK[0]].values[j]),
-                    PK[1]: str(df[PK[1]].values[j])})
+                    PK[1]: str(df[PK[1]].values[j])}
             if type(column1) == np.ndarray:
-                print(f'{name_table} value = {column1[j]}')
-                if type(column1[j]) == np.float64 or type(column1[j]) == np.float32:
-                    self.data[f'{type_error}'].append({f'{name_table} value': float(column1[j])})
-                elif type(column1[j]) == np.int:
-                    # self.data[f'{type_error}'].append({f'{name_table} value': int(column1)})
-                    self.data[f'{type_error}'].append({f'{name_table} value': int(column1[j])})
-            # self.data[f'{type_error}'].append({self.dict_json[f'{name_table} value']: column1})
-            #     self.data[f'{type_error}'].append({f'{name_table} value': column1[j]})
+                dict_value = self.conv_type_for_json(column1[j], name_table, False)
             else:
-                print(f'{name_table} value = {column1}')
-                # self.data[f'{type_error}'].append({self.dict_json[f'{name_table} value']: column1})
-                if type(column1) == np.float64 or type(column1) == np.float32 or type(column1) == float:
-                    self.data[f'{type_error}'].append({f'{name_table} value': float(column1)})
-                elif type(column1) == np.int:
-                    self.data[f'{type_error}'].append({f'{name_table} value': int(column1)})
-
-        # self.write_to_dict()
+                dict_value = self.conv_type_for_json(column1, name_table, False)
+        c = {**dict_, **dict_PK, **dict_value}
+        self.data[f'{type_error}'].append(c)
 
     def analysis_resassagnment(self, i):
         if i == 'resassignment':
-            sql_dubic_resassignment = f""" select {i}.id, {i}.id_resource, {i}.id_activity, {i}.id_project,{i}.id_wbs
+            sql_dubic_resassignment = f""" select {i}.id_resassignment, {i}.id_resource, {i}.id_activity, {i}.id_project,{i}.id_wbs
              from {i} """
             df_resassignment_dublicates = pd.read_sql_query(sql_dubic_resassignment, connection)
+            # df_resassignment_dublicates = pd.read_excel(PATH_READ)
             df1 = df_resassignment_dublicates[['id_resource', 'id_project', 'id_wbs', 'id_activity']]
             rm_dublicates_df = df1.drop_duplicates()
             if rm_dublicates_df.shape != df1.shape:
-                print('err')
+                print('Find_ diblicates')
+
                 df_find_dubl = df1.drop(rm_dublicates_df.index)
-                dubl = df_find_dubl[['id_project', 'id_resource', 'id_wbs', 'id_activity']].values
+                rm = df_find_dubl.drop_duplicates()
+                dubl = rm[['id_project', 'id_resource', 'id_wbs', 'id_activity']].values
+                # dubl = df_find_dubl[['id_project', 'id_resource', 'id_wbs', 'id_activity']].values
                 for i_dubl in dubl:
                     l = list(df1.loc[(df_resassignment_dublicates[['id_project', 'id_resource', 'id_wbs',
                                                                    'id_activity']] == i_dubl).all(axis=1)].index)
 
-                    ind_list = [df_resassignment_dublicates['id'][i_ind] for i_ind in l]
+                    ind_list = [df_resassignment_dublicates['id_resassignment'][i_ind] for i_ind in l]
                     #
-                    for i in range(0, len(ind_list) - 1, 2):
-                        logger.warning(f'Dublicate rows id = {ind_list[i]} and id = {ind_list[i + 1]}')
+                    print("ind_list = ", ind_list)
+                    print("l",l)
+                    print("len(ind_list)-1  = " ,len(ind_list)-1)
+                    for i in range(0, len(ind_list), 1):
+                        # print(l[i])
+                        print(i)
+                        self.print_name_table_row_PK(df_resassignment_dublicates, 0, 'resassignment', [], l[i],
+                                                     True,'id_resassignment', 'Dublicate rows in resassagment')
+                        # logger.warning(f'Dublicate rows id = {ind_list[i]} and id = {ind_list[i + 1]}')
 
     def analysis_two_tables(self, df, PK, name_table):
         column1 = df.values
@@ -343,7 +313,9 @@ class TYPE_Errors(object):
 
 dict_same_types = {int: [float], float: [int]}
 ALL = True
-
+BD = True
+PATH_TO_LOG = 'log/'
+# PATH_READ = 'D:\\work2\\analysis_exel_data\\analys_test.xlsx'
 DICT_TABLE_PK = {'resource': 'id_resource',
                  'activity': 'id_activity',
                  'udf_code_resassignment': 'id_resassignment',
@@ -357,80 +329,94 @@ DICT_TABLE_PK = {'resource': 'id_resource',
                  'dictionary_activity_code': 'id',
                  'dictionary_resource_code': 'id',
                  'actvrel': 'id_actvrel'}
+def chek_dictionary_activity_code_and_activity():
+    sql = '''
+            SELECT
+              activity.id_activity,
+              activity.activity_code,
+              activity.activity_name,
+              dictionary_activity_code."Dictionary_Name"
+            FROM
+              public.activity activity
+              INNER JOIN public.dictionary_activity_code dictionary_activity_code ON dictionary_activity_code.code = activity.activity_code
+            WHERE
+              dictionary_activity_code."Dictionary_Name" IN ('Виды работ', 'Конструктив для отчета');
+            '''
+    quary_ = pd.read_sql_query(sql, connection)
+    if quary_.shape[0] == 0:
+        logger.warning("Ни на одну работу не назначены коды!")
+
+
+def chek_all_tables(cursor):
+    cursor.execute("""SELECT relname FROM pg_class WHERE relkind='r'
+                                  AND relname !~ '^(pg_|sql_)';""")  # "rel" is short for relation.
+    tables_ = [i[0] for i in cursor.fetchall()]
+    # tables_ = ['resassignment', 'activity']
+
+    for i in tables_:
+        if i != 'ISR' and i != 'ISR_del' and i != 'ISR_pmc' and i != 'alembic_version':
+            if '_pmc' not in i and '_del' not in i:
+                print("Table = ", i)
+                sql_one_table = f""" select * from {i} """
+                d_all = pd.read_sql_query(sql_one_table, connection)
+                PK = DICT_TABLE_PK.get(i)
+                type_error = TYPE_Errors(d_all, i)
+                with open('/'.join((PATH_TO_LOG, 'log.txt')), 'a+') as outfile:
+                    outfile.write('{"Table": [ \n')
+                    outfile.write(f'"{i}", ')
+                type_error.analis_data_df(d_all, i, PK)
+                if i == 'resassignment':
+                    type_error.analysis_resassagnment(i)
+
+                try:
+                    i_pmc = i + '_pmc'
+                    # PK = DICT_TABLE_PK.get(i)
+                    if type(PK) == str:  # < 2:
+                        sql = f"""
+                                        select * from {i}, {i}_pmc
+                                        where {i}.{PK}={i}_pmc.{PK}
+                                        """
+                    else:
+                        sql = f"""
+                                        select * from {i}, {i}_pmc
+                                        where {i}.{PK[0]}={i}_pmc.{PK[0]} and {i}.{PK[1]}={i}_pmc.{PK[1]}
+                                         """
+                    print('ANALYSIS OF TWO TABLE ORIG AND ORIG_PMC')
+                    d_two_table = pd.read_sql_query(sql, connection)
+                    type_error.analysis_two_tables(d_two_table, PK, i)
+
+
+                except Exception as exp:
+                    print(f'Error {exp}')
+                    pass
+                type_error.write_to_dict()
+                with open('/'.join((PATH_TO_LOG, 'log.txt')), 'a+') as outfile:
+                        outfile.write(']}, \n')
+
 
 try:
-    # connection to bd
+    # if not BD:
+    #     rb = load_workbook(PATH_READ, read_only=False)  # , formatting_info=True)
+    #     sheet = rb['Лист1']
+    #     rb.active = 1
+    #     df = pd.read_excel(PATH_READ)
+    #     type_error = TYPE_Errors(df, 'resassignment')
+    #     type_error.analysis_resassagnment('resassignment')
+    #     type_error.write_to_dict()
+    # elif BD:
+        # connection to bd
     connection = psycopg2.connect(host=config.host, user=config.user, password=config.password, database=config.bd_name)
-    # sql = f"""
-    # select * from {NAME_TABLE}
-    # """
-    # d = pd.read_sql_query(sql, connection)
 
-    if ALL:
-        sql = '''
-        SELECT
-          activity.id_activity,
-          activity.activity_code,
-          activity.activity_name,
-          dictionary_activity_code."Dictionary_Name"
-        FROM
-          public.activity activity
-          INNER JOIN public.dictionary_activity_code dictionary_activity_code ON dictionary_activity_code.code = activity.activity_code
-        WHERE
-          dictionary_activity_code."Dictionary_Name" IN ('Виды работ', 'Конструктив для отчета');
-        '''
-        quary_ = pd.read_sql_query(sql, connection)
-        if quary_.shape[0] == 0:
-            logger.warning("Ни на одну работу не назначены коды!")
+    chek_dictionary_activity_code_and_activity()
 
-        cursor = connection.cursor()
-        cursor.execute("""SELECT relname FROM pg_class WHERE relkind='r'
-                          AND relname !~ '^(pg_|sql_)';""")  # "rel" is short for relation.
-        tables_ = [i[0] for i in cursor.fetchall()]
-        # tables_ = ['activity']
-
-        # ['resassignmentspred', 'udf_code_activity']
-        for i in tables_:
-            if i != 'ISR' and i != 'ISR_del' and i != 'ISR_pmc' and i != 'alembic_version':
-                if '_pmc' not in i and '_del' not in i:
-                    print("Table = ", i)
-                    sql_one_table = f""" select * from {i} """
-                    d_all = pd.read_sql_query(sql_one_table, connection)
-                    PK = DICT_TABLE_PK.get(i)
-                    type_error = TYPE_Errors(d_all, i)
-                    type_error.analis_data_df(d_all, i, PK)
-                    if i == 'resassignment':
-                        type_error.analysis_resassagnment(i)
-                    # type_error.write_to_dict()
-                    try:
-                        i_pmc = i + '_pmc'
-                        # PK = DICT_TABLE_PK.get(i)
-                        if type(PK) == str:  # < 2:
-                            sql = f"""
-                                select * from {i}, {i}_pmc
-                                where {i}.{PK}={i}_pmc.{PK}
-                                """
-                        else:
-                            sql = f"""
-                                select * from {i}, {i}_pmc
-                                where {i}.{PK[0]}={i}_pmc.{PK[0]} and {i}.{PK[1]}={i}_pmc.{PK[1]}
-                                 """
-                        print('ANALYSIS OF TWO TABLE ORIG AND ORIG_PMC')
-                        d_two_table = pd.read_sql_query(sql, connection)
-
-                        type_error.analysis_two_tables(d_two_table, PK, i)
-
-
-
-                    except Exception as exp:
-                        print(f'Error {exp}')
-                        pass
-                    # type_error.write_to_dict_begin()
-                    type_error.write_to_dict()
-                    # type_error.write_to_dict_end()
-
-    # print("all is ok")
-
+    cursor = connection.cursor()
+    if not os.path.exists(PATH_TO_LOG):
+        os.makedirs(PATH_TO_LOG)
+    with open('/'.join((PATH_TO_LOG, 'log.txt')), 'a+') as outfile:
+        outfile.write('[ \n')
+    chek_all_tables(cursor)
+    with open('/'.join((PATH_TO_LOG, 'log.txt')), 'a+') as outfile:
+        outfile.write('] \n')
 
 except Exception as exp:
     print(f'Error {exp}')
